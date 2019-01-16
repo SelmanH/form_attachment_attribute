7497ee2f4e873bc70668ea3db975837c366a27c5
Hichri Selman <s.hichri@tamkeentechnologie.sa>
2019-01-16 14:48:12 +0300

    [NEW] First commit

---
+++ form_attachment_attribute/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Form Attachment Attribute',
+    'version': '1.0',
+    'description': """
+        Add attribute attachment to form view for hiding the attachment action
+    """,
+    'author': 'Hichri Selman',
+    'category': 'Extra Tools',
+    'depends': [
+        'web','document'
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'installable': True,
+}
---
+++ form_attachment_attribute/static/src/js/attachment.js
@@ -0,0 +1,36 @@
+odoo.define('form_attachment_attribute', function (require) {
+"use strict";
+    var core = require('web.core');
+    var _t = core._t;
+    var Sidebar = require('web.Sidebar');
+    var Model = require('web.Model');
+    var session = require('web.session');
+    var user_id = false;
+
+    Sidebar.include({
+
+        init : function(){
+        this._super.apply(this, arguments);
+            var view = this.getParent();
+            if (view.fields_view && view.fields_view.type === "form") {
+                if(view.fields_view.arch.attrs.attachment !== 'false'){
+                    this.sections.splice(1, 0, { 'name' : 'files', 'label' : _t('Attachment(s)'), });
+                    this.items.files = [];
+                }
+            }
+        },
+        on_attachments_loaded: function(attachments) {
+            //to display number in name if more then one attachment which has same name.
+            var self = this;
+            _.chain(attachments)
+                 .groupBy(function(attachment) { return attachment.name; })
+                 .each(function(attachment){
+                     if(attachment.length > 1)
+                         _.map(attachment, function(attachment, i){
+                             attachment.name = _.str.sprintf(_t("%s (%s)"), attachment.name, i+1);
+                         });
+                  });
+            self._super(attachments);
+        },
+    });
+});
